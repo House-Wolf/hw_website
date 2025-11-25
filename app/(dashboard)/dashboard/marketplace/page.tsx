4c39787de454cea6cc92fe7f7f81066f533d1698
Chad Powell <brokkr.forgemaster+github@gmail.com>
2025-11-25 05:46:27 -0500

    added base file structure and pages to the website

---
+++ app/(dashboard)/dashboard/marketplace/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>marketplace</div>
+  )
+}
+
+export default page
---
+++ app/(root)/divisions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>House Wolf Divisions</div>
+  )
+}
+
+export default page
