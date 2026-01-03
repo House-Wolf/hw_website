7be43ece75b664c0319e910ce6b8c44e9e18a8f0
Chad Powell <brokkr.forgemaster+github@gmail.com>
2026-01-03 18:16:37 -0500

    update

--- app/(root)/fleet/page.tsx
+++ app/(root)/fleet/page.tsx
@@ -12,7 +12,7 @@ export default function FleetPage() {
                 iconSrc="/images/global/HWiconnew.png"
               />
       <section className="max-w-7xl mx-auto px-6 py-20">
-          <FleetyardsEmbed />
+          <FleetyardsEmbed  />
 
       </section>
     </div>
